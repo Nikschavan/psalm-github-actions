1ef6fce5541f761f7440dc654554c036b5e56c4a
Nikhil <email@nikhilchavan.com>
2020-05-23 11:21:33 +0530

    Setup dev-master image

--- command.php
+++ command.php
@@ -2,20 +2,21 @@
 
 require_once 'vendor/autoload.php';
 
-$psalm_versions      = json_decode( Requests::get( 'https://repo.packagist.org/p/vimeo/psalm.json' )->body, true )[ 'packages' ][ 'vimeo/psalm' ];
-$docker_template     = file_get_contents( 'Dockerfile.template' );
-$entrypoint_template = file_get_contents( 'entrypoint.sh.template' );
+$psalm_versions         = json_decode( Requests::get( 'https://repo.packagist.org/p/vimeo/psalm.json' )->body, true )['packages']['vimeo/psalm'];
+$docker_template        = file_get_contents( 'Dockerfile.template' );
+$entrypoint_template    = file_get_contents( 'entrypoint.sh.template' );
+$default_psalm_versions = [ 'dev-master' ];
 
 foreach ( $psalm_versions as $version => $package ) {
-    // Track only versions 3.0 and above
-    if ( version_compare( $version, '3.0', '>=') ) {
-        shell_exec( 'mkdir -p images/' . $version );
+	// Track only versions 3.0 and above
+	if ( version_compare( $version, '3.0', '>=' ) || in_array( $version, $default_psalm_versions, true ) === true ) {
+		shell_exec( 'mkdir -p images/' . $version );
 
-        $dockerfile = str_replace( '%%PSALM_VERSION%%', ':' . $version, $docker_template );
+		$dockerfile = str_replace( '%%PSALM_VERSION%%', ':' . $version, $docker_template );
 
-        write_file( 'images/' . $version . '/Dockerfile', $dockerfile );
+		write_file( 'images/' . $version . '/Dockerfile', $dockerfile );
         write_file( 'images/' . $version . '/entrypoint.sh', $entrypoint_template );
-    }
+	}
 }
 
 /**
